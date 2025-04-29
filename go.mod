49ce881f4e775dc2c2733cca88aec9fedbfa64ac
Keith Thibodeaux <keith@railscoder.net>
2025-04-29 18:27:50 -0400

    add score.Calculate

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/kthibodeaux/go-farkle
+
+go 1.24.1
---
+++ internal/score/score.go
@@ -0,0 +1,131 @@
+package score
+
+import (
+	"errors"
+	"fmt"
+)
+
+type roll map[int]int
+
+func Calculate(dieFaces []int) (int, error) {
+	roll, err := newRoll(dieFaces)
+	if err != nil {
+		return 0, err
+	}
+
+	if roll.ofAKind(6) != 0 {
+		return 3000, nil
+	}
+
+	if roll.hasTriplets() {
+		return 2500, nil
+	}
+
+	if roll.hasFourOfAKindAndAPair() || roll.hasThreePairs() || roll.hasStraight() {
+		return 1500, nil
+	}
+
+	score := 0
+
+	if face := roll.ofAKind(5); face != 0 {
+		score += 2000
+		roll[face] = 0
+	}
+
+	if face := roll.ofAKind(4); face != 0 {
+		score += 1000
+		roll[face] = 0
+	}
+
+	if face := roll.ofAKind(3); face != 0 {
+		if face == 1 {
+			score += 300
+		} else {
+			score += face * 100
+		}
+		roll[face] = 0
+	}
+
+	score += roll[1] * 100
+	score += roll[5] * 50
+
+	return score, nil
+}
+
+func newRoll(dieFaces []int) (roll, error) {
+	if err := validateDieFaces(dieFaces); err != nil {
+		return nil, err
+	}
+
+	roll := make(roll)
+	for _, die := range dieFaces {
+		roll[die]++
+	}
+
+	return roll, nil
+}
+
+func validateDieFaces(dieFaces []int) error {
+	if len(dieFaces) == 0 {
+		return errors.New("no dice")
+	}
+
+	if len(dieFaces) > 6 {
+		return errors.New("too many dice")
+	}
+
+	for _, die := range dieFaces {
+		if die < 1 || die > 6 {
+			return fmt.Errorf("invalid die face: %d", die)
+		}
+	}
+
+	return nil
+}
+
+func (r roll) ofAKind(targetCount int) int {
+	for face, count := range r {
+		if count == targetCount {
+			return face
+		}
+	}
+
+	return 0
+}
+
+func (r roll) hasTriplets() bool {
+	tripletCount := 0
+	for _, count := range r {
+		if count == 3 {
+			tripletCount++
+		}
+	}
+
+	return tripletCount == 2
+}
+
+func (r roll) hasFourOfAKindAndAPair() bool {
+	return r.ofAKind(4) != 0 && r.ofAKind(2) != 0
+}
+
+func (r roll) hasThreePairs() bool {
+	pairCount := 0
+	for _, count := range r {
+		if count == 2 {
+			pairCount++
+		}
+	}
+
+	return pairCount == 3
+}
+
+func (r roll) hasStraight() bool {
+	singlesCount := 0
+	for i := 1; i <= 6; i++ {
+		if r[i] == 1 {
+			singlesCount++
+		}
+	}
+
+	return singlesCount == 6
+}
