dc4bcdea8d89ee475ee3725ce00456b2fbb36b2a
koo04 <douglas.devicariis@prizepicks.com>
2025-05-07 17:11:09 -0400

    More progress

--- internal/menu/lobby.go
+++ internal/menu/lobby.go
@@ -1,10 +1,12 @@
 package menu
 
 import (
+	"slices"
 	"strconv"
 	"time"
 
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 )
 
 type lobbyModel struct {
@@ -60,7 +62,40 @@ func (m lobbyModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (m lobbyModel) View() string {
-	return "Lobby Name: " + m.name + "\n" +
-		"Players: " + strconv.Itoa(len(m.players)) + "\n" +
-		"Press Ctrl+C or q to quit."
+	fullPaneStyle := lipgloss.NewStyle().Width(m.width).Height(m.height).Align(lipgloss.Center, lipgloss.Center)
+	lobbyStyle := lipgloss.NewStyle().Margin(1, 2).Padding(1, 2).Border(lipgloss.NormalBorder(), true)
+	controlsStyle := lipgloss.NewStyle().Foreground(lipgloss.Color("#666666")).AlignHorizontal(lipgloss.Left).Width(m.width)
+	errorsStyle := lipgloss.NewStyle().Foreground(lipgloss.Color("#FF0000")).AlignHorizontal(lipgloss.Center)
+
+	l := []string{}
+	l = append(l, "Lobby: "+m.name)
+	l = append(l, "Players: "+strconv.Itoa(len(m.players)))
+
+	for i, player := range m.players {
+		if i == 0 {
+			player = player + " (Host)"
+		}
+		l = append(l, strconv.Itoa(i+1)+") "+player)
+	}
+
+	errors := ""
+	if slices.Contains(m.players, "") {
+		errors = "Waiting for players to join..."
+	}
+
+	lobbyRender := lobbyStyle.Render(
+		lipgloss.JoinVertical(
+			lipgloss.Left,
+			l...,
+		),
+	)
+
+	return lipgloss.JoinVertical(
+		lipgloss.Center,
+		fullPaneStyle.Render(
+			lobbyRender,
+		),
+		errorsStyle.Render(errors),
+		controlsStyle.Render("ESC to exit, Enter to start the game"),
+	)
 }
