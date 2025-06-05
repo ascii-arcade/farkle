63b4511bbdfb1c13b2a00eaf2f3a70a05b9f5c06
koo04 <douglas@devicariis.org>
2025-06-04 20:54:06 -0400

    Forgot to commit this

--- games/players.go
+++ games/players.go
@@ -11,13 +11,15 @@ import (
 var players = make(map[string]*Player)
 
 func NewPlayer(ctx context.Context, sess ssh.Session, langPref *language.LanguagePreference) *Player {
-	if player := getPlayer(sess); player != nil {
+	player, exists := players[sess.User()]
+	if exists {
 		player.UpdateChan = make(chan struct{})
 		player.connected = true
-		return player
+
+		goto RETURN
 	}
 
-	player := &Player{
+	player = &Player{
 		Name:               utils.GenerateName(langPref.Lang),
 		UpdateChan:         make(chan struct{}),
 		LanguagePreference: langPref,
@@ -27,19 +29,13 @@ func NewPlayer(ctx context.Context, sess ssh.Session, langPref *language.Languag
 	}
 	players[sess.User()] = player
 
+RETURN:
 	go func() {
 		<-player.ctx.Done()
 		player.connected = false
 	}()
 
 	return player
-}
-
-func getPlayer(sess ssh.Session) *Player {
-	if player, exists := players[sess.User()]; exists {
-		return player
-	}
-	return nil
 }
 
 func RemovePlayer(player *Player) {
